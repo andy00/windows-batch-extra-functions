9f7b1f143c5bd4eafde38f63aa12265b9cb91d1c
andrew <andy00@users.noreply.github.com>
2022-11-06 18:24:27 +0800

    Remove space
    Remove space!

--- create_string.bat
+++ create_string.bat
@@ -10,5 +10,19 @@ set string=%2
 ::I think this line removes any quotation marks with ~ notation at string=%%~g
 for /f "useback tokens=*" %%g in ('%string%') do set string=%%~g
 
+::To get string character counts
+::Create temporary file, including the quotation mark
+echo %2 > %TEMP%\tempfile.txt
+
+::For that text file, remove 5 unecessary characters.
+::2 quotes and 3 extra from windows.
+::Then assign %1_length for the character counts
+for %%g in ( %TEMP%\tempfile.txt ) do (
+    set /a %1_length=%%~zg - 5
+)
+
+::Delete the temporary file
+del %TEMP%\tempfile.txt
+
 ::Assign first variable %1 as string
 set %1=%string%
